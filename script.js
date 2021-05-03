e6e43917a4ba00e44d6f6b3b491575ee173a1ccb
WayneInwonHan <blissgram@gmail.com>
2021-05-02 23:28:00 -0500

    implement change button functions to change the shape and position

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 
 window.addEventListener('scroll', () => {
 
+    let ele = document.querySelector("#hero-button");
     const scrollable = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = window.scrollY;
 
-    console.log(scrolled);
-    console.log(scrollable);
-
-    if (Math.ceil(scrolled) > 360) {
-        alert("alert!")
+    if (Math.ceil(scrolled) > 350) {
+        changeFlt()
+        
+    }
+    if (Math.ceil(scrolled) < 350) {
+        changeBtn()
     }
 })
+
+function changeFlt() {
+    let ele = document.querySelector("#hero-button");
+    let txt = document.querySelector("#btn-text");
+    let icon = document.querySelector("#git-icon");
+
+    ele.style.left='90%';
+    ele.style.top='85%';
+    ele.style.width='75px';
+    ele.style.height='75px';
+    ele.style.backgroundColor="rgb(228, 228, 228)";
+    ele.style.borderRadius='50%';
+    ele.style.position='fixed';
+    txt.style.display="none";
+    icon.style.display="inline";
+}
+
+function changeBtn() {
+    let ele = document.querySelector("#hero-button");
+    let txt = document.querySelector("#btn-text");
+    let icon = document.querySelector("#git-icon");
+
+    ele.style.left='0%';
+    ele.style.top='0%';
+    ele.style.width='200px';
+    ele.style.height='50px';
+    ele.style.backgroundColor="black";
+    ele.style.borderRadius='25px';
+    txt.style.display="inline";
+    icon.style.display="none";
+}
